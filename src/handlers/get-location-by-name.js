04d2886ef3b5aa5b14624fa374f65c7e9a4e12e5
sohammondal <soham.pf.mondal@gmail.com>
2020-03-19 13:21:07 +0530

    implement getLocati onByName fn

--- src/handlers/get-location-by-name.js
+++ src/handlers/get-location-by-name.js
@@ -1,10 +1,45 @@
 const AWS = require('aws-sdk');
+AWS.config.update({
+    correctClockSkew: true
+});
 
 exports.getLocationByName = async (event) => {
+    const s3 = new AWS.S3();
+    const Bucket = 'location-data-test';
+    const params = event.pathParameters;
+    console.log(params);
+    if (params && params.name) {
 
-    return {
-        statusCode:200,
-        body: 'Success'
+        const locationName = params.name.toLowerCase();
+
+        try {
+
+            const resp = await s3.getObject({
+                Bucket,
+                Key: locationName
+            }).promise();
+
+            return {
+                statusCode: 200,
+                body: resp.Body.toString()
+            }
+
+        } catch (error) {
+            console.log('ERROR:', error.message);
+
+            if (error.code === "NoSuchKey") {
+                return {
+                    statusCode: 404,
+                    body: `Location '${locationName}' not found`
+                }
+            }
+
+            return {
+                statusCode: 500,
+                body: 'Internal Server Error'
+            }
+        }
+
     }
 
 }
