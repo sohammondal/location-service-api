3f18315678eeeac26e815e3d2f79519fe80ca5cc
sohammondal <soham.pf.mondal@gmail.com>
2020-03-19 22:20:13 +0530

    fix wrong longitude reference error

--- src/services/services.js
+++ src/services/services.js
@@ -5,21 +5,22 @@
  */
 exports.beelineDistanceCalculatorService = (src,dest) =>{
 
-    if ((src.lat == dest.lat) && (src.lon == dest.lon)) {
+    if ((src.lat == dest.lat) && (src.lng == dest.lng)) {
 		return 0;
 	}
 
     const radlat1 = Math.PI * src.lat/180;
     const radlat2 = Math.PI * dest.lat/180;
-    const theta = src.lon-dest.lon;
+    const theta = src.lng-dest.lng;
     const radtheta = Math.PI * theta/180;
-    const dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
     if (dist > 1) {
         dist = 1;
     }
     dist = Math.acos(dist);
     dist = dist * 180/Math.PI;
     dist = dist * 60 * 1.1515;
-    return dist * 1.609344; 
-    
+    dist = dist * 1.609344; //Km
+    return dist; 
+
 }
