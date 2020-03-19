85204d293ad11386a08db9d4f2b83b313c8c639f
sohammondal <soham.pf.mondal@gmail.com>
2020-03-19 12:18:37 +0530

    implement getAllLocations fn

--- src/handlers/get-all-locations.js
+++ src/handlers/get-all-locations.js
@@ -1,10 +1,29 @@
 const AWS = require('aws-sdk');
-
+AWS.config.update({
+    correctClockSkew: true
+});
 exports.getAllLocations = async (event) => {
+    
+    const s3 = new AWS.S3();
+    const Bucket = 'location-data-test';
 
-    return {
-        statusCode:200,
-        body: 'Success'
+    try {
+        const resp = await s3.listObjectsV2({Bucket}).promise();
+        const locations = resp.Contents.map(content=>content.Key);
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify({locations})
+        }
+
+    } catch (error) {
+
+        console.log('ERROR:',error.message);
+        return {
+            statusCode:500,
+            body: 'Internal Server Error'
+        }
+        
     }
 
 }
